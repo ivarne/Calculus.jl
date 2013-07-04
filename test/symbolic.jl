be9b014596297b559f570000270669765c6008e1
ivarne <ivarne@gmail.com>
2013-07-04 18:08:43 +0200

    Tests for differentiate with simplified expressions
    It was hard to write test for differentiate() because the writer had to
    know exactly how the expression would be simplified. Now a utility
    function is used to simplify both the automatically differentiated
    expression and compare the simplified version. The error message if the
    assertion fails is also better.

--- test/symbolic.jl
+++ test/symbolic.jl
@@ -17,43 +17,49 @@ eqsimp(a,b) = isequal(simplify(a),b)
 #
 # Univariate Calculus
 #
-
-@assert isequal(differentiate(:(2), :x), 0)
-@assert isequal(differentiate(:(x), :x), 1)
-@assert isequal(differentiate(:(x + x), :x), 2)
-@assert isequal(differentiate(:(x - x), :x), 0)
-@assert isequal(differentiate(:(2 * x), :x), 2)
-@assert isequal(differentiate(:(2 / x), :x), :(-2 / x^2))
-@assert isequal(differentiate(:(x / 2), :x), 0.5)
-@assert isequal(differentiate(:(sin(x) / x), :x), :((cos(x) * x - sin(x)) / x^2))
-@assert isequal(differentiate(:(x * 2), :x), 2)
-@assert isequal(differentiate(:(a * x), :x), :a)
-@assert isequal(differentiate(:(x * a), :x), :a)
-@assert isequal(differentiate(:(x ^ 2), :x), :(2 * x))
-@assert isequal(differentiate(:(a * x ^ 2), :x), :(a * (2 * x)))
-@assert isequal(differentiate(:(2 ^ x), :x), :(*(^(2, x), 0.6931471805599453)))
-@assert isequal(differentiate(:(sin(x)), :x), :(cos(x)))
-@assert isequal(differentiate(:(cos(x)), :x), :(*(-1,sin(x))))  # needs a better simplify
-@assert isequal(differentiate(:(tan(x)), :x), :(1 + tan(x)^2))
-@assert isequal(differentiate(:(exp(x)), :x), :(exp(x)))
-@assert isequal(differentiate(:(log(x)), :x), :(1 / x))
-@assert isequal(differentiate(:(sin(x) + sin(x)), :x), :(cos(x) + cos(x)))
-@assert isequal(differentiate(:(sin(x) - cos(x)), :x), :(-(cos(x),*(-1,sin(x))))) # Simplify -(a, -(b)) => +(a, b)
-@assert isequal(differentiate(:(x * sin(x)), :x), :(sin(x) + x * cos(x)))
-@assert isequal(differentiate(:(x / sin(x)), :x), :((sin(x) - x * cos(x)) / (sin(x)^2)))
-@assert isequal(differentiate(:(sin(sin(x))), :x), :(*(cos(x),cos(sin(x)))))
-@assert isequal(differentiate(:(sin(cos(x) + sin(x))), :x), :(*(+(*(-1,sin(x)),cos(x)),cos(+(cos(x),sin(x)))))) # Clean this up
-@assert isequal(differentiate(:(exp(-x)), :x), :(*(-1,exp(-(x))))) # Simplify this to -(exp(-x))
-@assert isequal(differentiate(:(log(x^2)), :x), :(/(*(2,x),^(x,2))))
-@assert isequal(differentiate(:(x^n), :x), :(*(n, ^(x, -(n, 1)))))
-@assert isequal(differentiate(:(n^x), :x), :(*(^(n, x), log(n))))
-@assert isequal(differentiate(:(n^n), :x), 0)
+function assert_differentiate(y, x, y_der)
+	y_der_sym = differentiate(y,x)
+	if isequal(simplify(y_der_sym),simplify(y_der))
+		return nothing
+	end
+	error("differentiate(",y,", ",x,") = ",y_der_sym," is not equivalent to ",y_der)
+end
+assert_differentiate(:(2), :x, 0)
+assert_differentiate(:(x), :x, 1)
+assert_differentiate(:(x + x), :x, 2)
+assert_differentiate(:(x - x), :x, 0)
+assert_differentiate(:(2 * x), :x, 2)
+assert_differentiate(:(2 / x), :x, :(-2 / x^2))
+assert_differentiate(:(x / 2), :x, 0.5)
+assert_differentiate(:(sin(x) / x), :x, :((cos(x) * x - sin(x)) / x^2))
+assert_differentiate(:(x * 2), :x, 2)
+assert_differentiate(:(a * x), :x, :a)
+assert_differentiate(:(x * a), :x, :a)
+assert_differentiate(:(x ^ 2), :x, :(2 * x))
+assert_differentiate(:(a * x ^ 2), :x, :(a * (2 * x)))
+assert_differentiate(:(2 ^ x), :x, :(*(^(2, x), 0.6931471805599453)))
+assert_differentiate(:(sin(x)), :x, :(cos(x)))
+assert_differentiate(:(cos(x)), :x, :(*(-1,sin(x))))  # needs a better simplify
+assert_differentiate(:(tan(x)), :x, :(1 + tan(x)^2))
+assert_differentiate(:(exp(x)), :x, :(exp(x)))
+assert_differentiate(:(log(x)), :x, :(1 / x))
+assert_differentiate(:(sin(x) + sin(x)), :x, :(cos(x) + cos(x)))
+assert_differentiate(:(sin(x) - cos(x)), :x, :(-(cos(x),*(-1,sin(x))))) # Simplify -(a, -(b)) => +(a, b)
+assert_differentiate(:(x * sin(x)), :x, :(sin(x) + x * cos(x)))
+assert_differentiate(:(x / sin(x)), :x, :((sin(x) - x * cos(x)) / (sin(x)^2)))
+assert_differentiate(:(sin(sin(x))), :x, :(*(cos(x),cos(sin(x)))))
+assert_differentiate(:(sin(cos(x) + sin(x))), :x, :(*(+(*(-1,sin(x)),cos(x)),cos(+(cos(x),sin(x)))))) # Clean this up
+assert_differentiate(:(exp(-x)), :x, :(*(-1,exp(-(x))))) # Simplify this to -(exp(-x))
+assert_differentiate(:(log(x^2)), :x, :(/(*(2,x),^(x,2))))
+assert_differentiate(:(x^n), :x, :(*(n, ^(x, -(n, 1)))))
+assert_differentiate(:(n^x), :x, :(*(^(n, x), log(n))))
+assert_differentiate(:(n^n), :x, 0)
 
 #
 # Multivariate Calculus
 #
 
-@assert isequal(differentiate(:(sin(x) + sin(y)), [:x, :y]), [:(cos(x)), :(cos(y))])
+assert_differentiate(:(sin(x) + sin(y)), [:x, :y], [:(cos(x)), :(cos(y))])
 
 # TODO: Get the generalized power rule right.
 # @assert isequal(differentiate(:(sin(x)^2), :x), :(2 * sin(x) * cos(x)))
@@ -63,7 +69,7 @@ eqsimp(a,b) = isequal(simplify(a),b)
 #
 
 # @assert isequal(differentiate("sin(x) + cos(x)^2"), :(+(cos(x),*(2,cos(x)))))
-@assert isequal(differentiate("x + exp(-x) + sin(exp(x))", :x), :(+(1,*(-1,exp(-(x))),*(exp(x),cos(exp(x))))))
+assert_differentiate("x + exp(-x) + sin(exp(x))", :x, :(+(1,*(-1,exp(-(x))),*(exp(x),cos(exp(x))))))
 
 # TODO: Make these work
 # differentiate(:(sin(x)), :x)(0.0)
